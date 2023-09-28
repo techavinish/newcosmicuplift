d230481de8a2c66b5d335f2a3bb25ae8393a12d7
Avinish Kumar <55577822+techavinish@users.noreply.github.com>
2023-09-28 07:25:23 +0530

    Add files via upload

---
+++ env.ts
@@ -0,0 +1,6 @@
+export const EnvVars = {
+  SITE_NAME: 'cosmicuplift',
+  OG_IMAGES_URL: 'https://cosmicuplift.com/Potential.jpeg',
+  URL: 'https://cosmicuplift.com/',
+  MAILCHIMP_SUBSCRIBE_URL: 'https://cosmicuplift.us17.list-manage.com/subscribe/post?u=de55387f0fa825d843ca38a7c&amp;id=afe683cd45&amp;f_id=00d569e0f0',
+};
